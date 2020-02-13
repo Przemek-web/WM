00778e5fd453c58752da22e44da2ee6b9545741e
Przemek <gornik.przemek@gmail.com>
2020-02-13 11:18:08 +0100

    commit

---
+++ target/generated-sources/annotations/pl/lodz/p/it/isdp/wm/model/WarehouseAccount_.java
@@ -0,0 +1,11 @@
+package pl.lodz.p.it.isdp.wm.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-08T13:35:30")
+@StaticMetamodel(WarehouseAccount.class)
+public class WarehouseAccount_ extends Account_ {
+
+
+}
